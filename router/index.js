6c42c51af6375b23ed95fbcc3d62b6c9af2f6ed6
Alexandre <alexandrepereiraassano@gmail.com>
2023-11-27 20:24:01 +0900

    req.user.created_date fix(7)

--- router/index.js
+++ router/index.js
@@ -3,7 +3,7 @@ const router = require("express").Router();
 router.get("/", (req, res) => {
   if(req.user){
     if(req.user.certification == null){
-      res.render("./signup/completed.ejs", { email: req.user.email });
+      res.render("./signup/completed.ejs", { email: req.user.email, date: req.user.created_date });
     }
   }
 
