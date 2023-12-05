6a185251d5a2ca223abdfbb95ff05989f0b8aff1
Alexandre <alexandrepereiraassano@gmail.com>
2023-12-05 21:47:31 +0900

    keypress type modal window rules auto add

--- public/js/keypresstype.js
+++ public/js/keypresstype.js
@@ -6,13 +6,39 @@
 
 /* ----- obtaining the required HTML elements ends here ----- */
 
-/* ----- modal window action starts here ----- */
+/* ----- ----- ----- modal window action starts here ----- ----- ----- */
 const keypresstypeBtn = document.getElementById("keypresstype_btn");
-const keypressCheckWindow = document.getElementById("keypress-check-window");
-const keypressCheckWindowCloseBtn = document.querySelector(".keypress-check-window-close-btn");
+const checkWindow = document.getElementById("check-window");
+const checkWindowCloseBtn = document.querySelector(".check-window-close-btn");
 const checkItems = document.getElementById("check_items");
 const gameStartBtn = document.getElementById("game-start-btn");
+const keypressTypeRules = [
+  "Use English keyboard only",
+  "The time limit is 60 seconds",
+  "Invalid string input will not be accepted",
+  "All characters entered will be subject to evaluation",
+  "Uppercase and lowercase input is sensitive",
+  "Game starts after 3 seconds after pressing the button",
+  "Score will be displayed after the game ends"
+];
 
+/* ----- add rules in modal window ----- */
+const addRules = rules => {
+  for(let i = 0;i < rules.length;i++){
+    const list = document.createElement("li");
+    list.textContent = rules[i];
+    checkItems.appendChild(list);
+  }
+};
+
+/* ----- remove rules in modal window ----- */
+const removeRules = _ => {
+  console.log(checkItems.children.length);
+  while(checkItems.firstChild)
+    checkItems.removeChild(checkItems.firstChild);
+};
+
+/* ----- check items effect ----- */
 const checkEffect = _ => {
   let itemNumber = 0;
   const checkIntervalTime = 500;
@@ -21,6 +47,7 @@ const checkEffect = _ => {
     if(itemNumber === checkItems.children.length - 1){
       setTimeout(_ => {
         gameStartBtn.disabled = false;
+        checkWindowCloseBtn.classList.add("show");
       }, checkIntervalTime);
       clearInterval(makeCheck);
     }
@@ -29,19 +56,34 @@ const checkEffect = _ => {
   }, checkIntervalTime);
 };
 
+/* ----- check items effect return(when click close button) ----- */
 const disableCheckEffect = _ => {
-  for(let i = 0;i < checkItems.children.length; i++ )
+  for(let i = 0;i < checkItems.children.length - 1; i++ )
     checkItems.children[i].classList.remove("li_effect");
   gameStartBtn.disabled = true;
+  checkWindowCloseBtn.classList.remove("show");
 };
 
+/* ----- keypress type button action ----- */
 keypresstypeBtn.addEventListener("click", _ => {
-  keypressCheckWindow.classList.remove("hidden");
+  addRules(keypressTypeRules);
+  checkWindow.classList.remove("hidden");
   checkEffect();
 });
 
-keypressCheckWindowCloseBtn.addEventListener("click", _ => {
-  keypressCheckWindow.classList.add("hidden");
+/* ----- close button action ----- */
+checkWindowCloseBtn.addEventListener("click", _ => {
+  checkWindow.classList.add("hidden");
   disableCheckEffect();
+  removeRules();
 });
-/* ----- modal window action ends here ----- */
+/* ----- ----- ----- modal window action ends here ----- ----- ----- */
+
+/* ----- game start action starts here ----- */
+gameStartBtn.addEventListener("click", _ => {
+  gameStartBtn.disabled = true;
+  checkWindow.classList.add("hidden");
+  disableCheckEffect();
+  removeRules();
+});
+/* ----- game start action ends here ----- */
