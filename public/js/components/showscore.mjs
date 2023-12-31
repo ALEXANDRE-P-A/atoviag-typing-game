3db309ea5614fd21ffc07e7a65303cbb9566c525
Alexandre <alexandrepereiraassano@gmail.com>
2023-12-31 12:07:28 +0900

    src js files modulization(11) show score(1)

--- public/js/components/showscore.mjs
+++ public/js/components/showscore.mjs
@@ -7,12 +7,43 @@ const checkWindowDiv = document.querySelector("#check-window div");
 const checkWindowOl = document.querySelector("#check-window ol");
 const modalWindowBtnArea = document.querySelector(".modal-window-btn-area");
 
+const transformArray = array => {
+  let response = "";
+  for(let i = 0;i < array.length-1;i++){
+    response += `
+      <tr>
+        <td>${array[i][0]}</td>
+        <td>${array[i][1]}</td>
+      </tr>
+    `;
+  }
+
+  if(array[array.length-1].length > 2){
+    const lastText = array[array.length-1][0].split("_")[0];
+    response += `
+      <tr>
+        <td><span>${lastText}</span><span style="opacity: .3;">${array[array.length-1][2]}</span></td>
+        <td>${array[array.length-1][1]}</td>
+      </tr>
+    `;
+  } else {
+    response += `
+      <tr>
+        <td>${array[array.length-1][0]}</td>
+        <td>${array[array.length-1][1]}</td>
+      </tr>
+    `;
+  }
+  
+
+  return response;
+}
+
 const showScore = _ => {
   const record = getUserRecord();
 
   checkWindowTitle.textContent = "Congratulations ";
   checkWindow.classList.remove("hidden");
-  checkWindow.classList.add("width_fit-content");
 
   checkWindowOl.classList.add("hidden");
   checkWindowDiv.classList.add("center");
@@ -24,35 +55,23 @@ const showScore = _ => {
   li.innerHTML = rank(record.keypress_time, record.score);
   ul.appendChild(li);
 
-  const details = document.createElement("li");
+  const details = document.createElement("details");
   details.innerHTML = `
-    <details>
-      <summary>Score details</summary>
-      <table class="score_details_table">
-        <thead>
-          <tr>
-            <th>Text</th>
-            <th>Time</th>
-          </tr>
-        </thead>
-        <tbody>
-          ${record.gradesArray}
-          ${record.gradesArray.length}
-    
-          <tr>
-            <td>Apple</td>
-            <td>3.21</td>
-          </tr>
-          <tr>
-            <td>Google</td>
-            <td>23.33</td>
-          </tr>
-        </tbody>
-      </table>
-    </details>
+    <summary>Score details</summary>
+    <table class="score_details_table">
+      <thead>
+        <tr>
+          <th>Text</th>
+          <th>Time</th>
+        </tr>
+      </thead>
+      <tbody>    
+        ${transformArray(record.gradesArray)}
+      </tbody>
+    </table>
   `;
 
-  ul.appendChild(details);
+  checkWindowDiv.appendChild(details);
 
   modalWindowBtnArea.innerHTML = `
     <span class="score_check material-symbols-outlined">done</span>
