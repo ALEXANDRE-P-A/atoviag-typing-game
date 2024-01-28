35c87ae4f7b451c89c194d023b950ef3f525202f
Alexandre <alexandrepereiraassano@gmail.com>
2024-01-28 18:46:19 +0900

    fetch results in MySQL

---
+++ lib/database/sql/INSERT_RECORD.sql
@@ -0,0 +1,4 @@
+INSERT INTO
+  records (`id`, `email`, `device`, `date`, `playtype`, `class`, `keypresstime`, `score`, `typeaccurancy`, `gradesarray`)
+VALUES 
+  (?, ?, ?, now(), ?, ?, ?, ?, ?, ?)
--- router/fetchresult.js
+++ router/fetchresult.js
@@ -1,6 +1,12 @@
 const router = require("express").Router();
 
+const { MySQLClient, sql } = require("../lib/database/client.js");
+
 router.post("/", async (req, res, next) => {
+  let transaction;
+  let string_to_store = "";
+
+  const test = "test";
   const { 
     id,
     email,
@@ -10,7 +16,8 @@ router.post("/", async (req, res, next) => {
     rank,
     keypress_time,
     score,
-    type_accurancy
+    type_accurancy,
+    gradesArray
   } = req.body;
 
   console.log(`id : ${id}`);
@@ -22,6 +29,25 @@ router.post("/", async (req, res, next) => {
   console.log(`keypress_time : ${keypress_time}`);
   console.log(`score : ${score}`);
   console.log(`type_accurancy : ${type_accurancy}`);
+  console.log(`grades_array : ${gradesArray}`);
+  console.log(`type of gradeArray : ${gradesArray}`);
+  console.log(`gradesArray Length : ${gradesArray.length}`);
+  console.log(`gradesArray first element : ${gradesArray[0]}`);
+  console.log(`gradesArray last element : ${gradesArray[gradesArray.length - 1]}`);
+  gradesArray.forEach(element => string_to_store += element + ",");
+  console.log(`string to store : ${string_to_store}`);
+
+  try{
+    transaction = await MySQLClient.beginTransaction();
+    transaction.executeQuery(
+      await sql("INSERT_RECORD"),
+      [id, email, device, play_type, rank, keypress_time, score, type_accurancy, string_to_store]
+    );
+    await transaction.commit();
+  } catch(err){
+    await transaction.rollback();
+    next(err);
+  }
 });
 
 module.exports = router;
